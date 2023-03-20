b1e05bf0a8d46fe94416cb2b5a2dc36cd849c86b
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-03-20 23:17:10 +0600

    paralelogram added

--- js/calculate.js
+++ js/calculate.js
@@ -5,19 +5,19 @@ function userInputValue(userInput) {
   const userInputValueInNumber = parseFloat(userInputValueString);
   return userInputValueInNumber;
 }
-
-function displayResult(result, value, name) {
-  const container = document.getElementById(result);
+// function for display result
+function displayResult(id, result, name) {
+  console.log(result);
+  const container = document.getElementById(id);
   const li = document.createElement("li");
   li.innerHTML = `
     <ul>
-        <li>${name}:  ${value} <button  style="margin-left: 10px;" class="text-white bg-blue-600 px-2 py-1 rounded font-semibold" id="myButton">Convert m^2</button> </li><br>
+        <li>${name}:  ${result}cm^2 <button  style="margin-left: 10px;" class="text-white bg-blue-600 px-2 py-1 rounded font-semibold" id="myButton">Convert m^2</button> </li><br>
     </ul>
     
 `;
   container.appendChild(li);
 }
-
 document.getElementById("btn-triangle").addEventListener("click", function () {
   const trianle = document.getElementById("triangle");
   const triangleName = trianle.innerText;
@@ -27,13 +27,38 @@ document.getElementById("btn-triangle").addEventListener("click", function () {
   const result = 0.5 * valueOfB * valueOfH;
   displayResult("display-result", result, triangleName);
 });
-
+// forrectangle calculation
 document.getElementById("btn-rectangle").addEventListener("click", function () {
   const rectangle = document.getElementById("rectangle");
   const rectangleName = rectangle.innerText;
 
-  const valueOfB = userInputValue("value-of-b");
-  const valueOfH = userInputValue("value-of-h");
+  const valueOfB = userInputValue("value-of-w");
+  const valueOfH = userInputValue("value-of-l");
   const result = valueOfB * valueOfH;
   displayResult("display-result", result, rectangleName);
 });
+
+// for Parallelogram calculation
+
+document
+  .getElementById("btn-parallelogram")
+  .addEventListener("click", function () {
+    const parallelogram = document.getElementById("parallelogram");
+    const parallelogramName = parallelogram.innerText;
+
+    const valueOfB = userInputValue("value-b");
+    const valueOfH = userInputValue("value-h");
+    const result = valueOfB * valueOfH;
+    displayResult("display-result", result, parallelogramName);
+  });
+
+document
+  .getElementById("btn-parallelogram")
+  .addEventListener("click", function () {
+    const parallelogram = document.getElementById("parallelogram");
+    const parallelogramName = parallelogram.innerText;
+    const valueofb = 10;
+    const valueofh = 12;
+    const result = valueofb * valueofh;
+    displayResult("display-result", result, parallelogramName);
+  });
